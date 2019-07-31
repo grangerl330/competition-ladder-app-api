09f2a85b8ef4805d0626d3e1eb39f85b11b6347e
grangerl330 <grangerl330@gmail.com>
2019-07-30 21:51:15 -0400

    added sessions controller to handle login

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,33 @@
+class Api::SessionsController < ApplicationController
+  
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Email or Password"
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "No one logged in "
+      }
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: {
+      notice: "Successfully logged out"
+    }
+  end
+
+end
