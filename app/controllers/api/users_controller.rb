732f60b5ab4aa661e07c3bc23fc107cfdff77c74
grangerl330 <grangerl330@gmail.com>
2019-07-30 22:30:11 -0400

    added methods to users controller for creating and updating users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,32 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+      session[:user_id] = @user.id
+    else
+      render json: {error: "Unable to create new user"}, status: 400
+    end
+  end
+
+  def update
+    @user = User.find_by_id(params[:user][:id])
+
+    if @user.update(user_params)
+      render json:  @user
+    else
+      render json: {
+        error: "Failed To Update"
+      }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
 end
