261638a55e38f2bb13821bfeb28090dbd0880885
grangerl330 <grangerl330@gmail.com>
2019-07-30 23:23:08 -0400

    added methods to ladders controller

--- app/controllers/api/ladders_controller.rb
+++ app/controllers/api/ladders_controller.rb
@@ -1,2 +1,47 @@
 class Api::LaddersController < ApplicationController
+
+  def index
+    @ladders = current_user.ladders
+    render json: @ladders
+  end
+
+  def create
+    @ladder = Ladder.new(ladder_params)
+
+    if @ladder.save
+      render json: @ladder
+    else
+      render json: {
+        error: "This ladder failed to save"
+      }
+    end
+  end
+
+  def update
+    @ladder = Ladder.find_by_id(params[:ladderId])
+
+    if @ladder.update(ladder_params)
+      render json: @ladder
+    else
+      render json: {
+        error: "This Ladder Failed To Update"
+      }
+    end
+  end
+
+  def destroy
+    @ladder = Ladder.find_by_id(params[:ladderId])
+    @ladder.destroy
+
+    render json: {
+      notice: "Ladder Successfully Deleted",
+      ladderId: params[:ladderId]
+    }
+  end
+
+  private
+
+  def ladder_params
+    params.require(:ladder).permit(:size)
+  end
 end
