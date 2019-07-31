07d16b8e2affe56ffaf310946f0bf43f47e1a63f
grangerl330 <grangerl330@gmail.com>
2019-07-30 22:14:59 -0400

    addeds routes for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/api/get_current_user', to: 'api/sessions#get_current_user'
+  post '/api/login', to: 'api/sessions#create'
+  delete '/api/logout', to: 'api/sessions#destroy'
+
   namespace :api do
     resources :matches
     resources :players
