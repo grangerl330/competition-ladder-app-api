628bb9ffa94200f4dadfa3f085791c25992a9b59
grangerl330 <grangerl330@gmail.com>
2019-08-02 21:46:42 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@
 User.create(username: "user1", email: "user1@gmail.com", password: "password")
 
 #Ladders
-Ladder.create(title: "Ladder 1", size: 10, user_id: 1)
-Ladder.create(title: "Ladder 2", size: 15, user_id: 1)
-Ladder.create(title: "Ladder 3", size: 20, user_id: 1)
+Ladder.create(title: "Summer 2019", size: 10, user_id: 1)
+Ladder.create(title: "Fall 2019", size: 15, user_id: 1)
 
-#Players - All names are fictional
+# Players
+# Summer 2019
 Player.create(first_name: "Bob", last_name: "Jones", match_record: "0-0", ladder_id: 1, ladder_spot: 1)
 Player.create(first_name: "Sam", last_name: "Smith", match_record: "0-0", ladder_id: 1, ladder_spot: 2)
 Player.create(first_name: "Billy", last_name: "Bobby", match_record: "0-0", ladder_id: 1, ladder_spot: 3)
@@ -25,3 +25,15 @@ Player.create(first_name: "Justin", last_name: "Brown", match_record: "0-0", lad
 Player.create(first_name: "Jimmy", last_name: "Davis", match_record: "0-0", ladder_id: 1, ladder_spot: 8)
 Player.create(first_name: "William", last_name: "Wilson", match_record: "0-0", ladder_id: 1, ladder_spot: 9)
 Player.create(first_name: "Roger", last_name: "Morris", match_record: "0-0", ladder_id: 1, ladder_spot: 10)
+
+# Fall 2019
+Player.create(first_name: "Roger", last_name: "Federer", match_record: "0-0", ladder_id: 2, ladder_spot: 1)
+Player.create(first_name: "Rafael", last_name: "Nadal", match_record: "0-0", ladder_id: 2, ladder_spot: 2)
+Player.create(first_name: "Novak", last_name: "Djokovic", match_record: "0-0", ladder_id: 2, ladder_spot: 3)
+Player.create(first_name: "Dominic", last_name: "Theim", match_record: "0-0", ladder_id: 2, ladder_spot: 4)
+Player.create(first_name: "Alexander", last_name: "Zverev", match_record: "0-0", ladder_id: 2, ladder_spot: 5)
+Player.create(first_name: "Stefanos", last_name: "Tsitsipas", match_record: "0-0", ladder_id: 2, ladder_spot: 6)
+Player.create(first_name: "Kei", last_name: "Nishikori", match_record: "0-0", ladder_id: 2, ladder_spot: 7)
+Player.create(first_name: "Karen", last_name: "Khachanov", match_record: "0-0", ladder_id: 2, ladder_spot: 8)
+Player.create(first_name: "Fabio", last_name: "Fognini", match_record: "0-0", ladder_id: 2, ladder_spot: 9)
+Player.create(first_name: "Danil", last_name: "Medvedev", match_record: "0-0", ladder_id: 2, ladder_spot: 10)
